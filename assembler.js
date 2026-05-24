b51c8a7b96c96d329f0222c7c9ec0be2c7961282
galastiaos <71029476+galastiaos@users.noreply.github.com>
2026-05-24 11:44:02 -0400

    Update assembler.js
    see if im even doing anything

--- assembler.js
+++ assembler.js
@@ -50,7 +50,8 @@ function SimulatorWidget(node) {
     $node.find('.stepButton').click(simulator.debugExec);
     $node.find('.gotoButton').click(simulator.gotoAddr);
     $node.find('.notesButton').click(ui.showNotes);
-    $node.find('.saveButton').click(ui.saveCode);
+    //$node.find('.saveButton').click(ui.saveCode);
+    $node.find('.notesButton').click(alert("hello world"));
     $node.find('.code').keypress(simulator.stop);
     $node.find('.code').keypress(ui.initialize);
     $(document).keypress(memory.storeKeypress);
